8424d6f57e964842fa493a700be957eb97808a1f
shirleylidianec@hotmail.com <shirleylidianec@hotmail.com>
2022-04-20 21:59:09 -0300

    variaveis e tipos de dados

---
+++ aula1/tiposdedados.js
@@ -0,0 +1,23 @@
+
+// tipo number
+let numero = 35;
+let preco = 150.65;
+
+//tipo string
+let nome = "Danilo";
+let disciplina = "Javascript";
+
+// tipo boolean
+let cursoLegal = true;
+let professorLegal = false
+
+//tipos especiais
+let divisaoImperfeita = "35a" /2
+
+let temperatura;
+temperatura = 22;
+
+let orcamento = null;
+
+
+console.log(null)
---
+++ aula1/variaveis.js
@@ -0,0 +1,13 @@
+let olaMundo = "Hello World!!"
+olaMundo = "Olá Mundo!!!"
+olaMundo = "Olá"
+
+var nome = "Shirley"
+nome = "Shirley Claro"
+
+const meuProfessor = "Danilo Santos"
+// meuProfessor = "Danilo" (ERRO: não é possível reatribuir o valor à uma constante.)
+
+console.log(olaMundo)
+console.log(nome)
+console.log(meuProfessor)
