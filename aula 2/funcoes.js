8a9f78254a13064f860f820c1ee012cd6b29faf2
shirleylidianec@hotmail.com <shirleylidianec@hotmail.com>
2022-04-25 12:18:00 -0300

    Aula 2 - Funções e Operadores

---
+++ aula 2/funcoes.js
@@ -0,0 +1,6 @@
+function somar (preco,valorReajuste){
+    return preco + valorReajuste;
+}
+console.log(somar(300,30));
+console.log(somar(250,15));
+console.log(somar(125,12));
---
+++ aula 2/operadores.js
@@ -0,0 +1,36 @@
+//operador de soma
+let idade1 = 31;
+let idade2 = 25;
+let resultado = idade1 + idade2;
+
+console.log(resultado)
+
+//operador de subtraçao
+let valor1 = 50;
+let valor2 = 30;
+let subtracao = valor1 - valor2
+
+console.log(subtracao)
+
+//operador de multiplicação
+let preco1 = 100;
+let preco2 = 250;
+let muliplicacao  = preco1 * preco2
+
+console.log(preco1 * preco2)
+
+//operador de divisão
+let preco3 = 350;
+let preco4 = 35;
+let divisao = preco3 / preco4;
+
+console.log(divisao)
+
+//operador módulo (retorna o resto de uma divisão)
+
+let produto1 = 9;
+let produto2 = 2;
+let produtoFinal = produto1 % produto2;
+
+console.log(produtoFinal)
+
